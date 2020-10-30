e627e1b8f5e7466f8da1d8ef488a3b3693e1e9a8
Mehmet Niyazi Yarar <mehmetnyarar@gmail.com>
2020-10-30 14:51:43 +0600

    feat(app): initial app
    fix #5

---
+++ packages/app/README.md
@@ -0,0 +1,10 @@
+# README
+
+[![runs with expo](https://img.shields.io/badge/Runs%20with%20Expo-4630EB.svg?style=flat-square&logo=EXPO&labelColor=f3f3f3&logoColor=000)](https://expo.io/)
+
+This package provides multi-platform front-end applications.
+
+Currently available platforms are:
+
+- Android
+- iOS
---
+++ packages/app/__tests__/mobile/error-boundary.spec.tsx
@@ -0,0 +1,40 @@
+import { render } from '@testing-library/react-native'
+import React from 'react'
+import { Text } from 'react-native'
+import { ErrorBoundary } from '~/error-boundary'
+
+// #region Setup
+
+const Child = () => <Text>You should see me!</Text>
+const ChildWithError = () => {
+  throw new Error('There is an error!')
+}
+
+// #endregion
+
+describe('ErrorBoundary', () => {
+  it('should render the children', () => {
+    const { queryByText } = render(
+      <ErrorBoundary>
+        <Child />
+      </ErrorBoundary>
+    )
+    expect(queryByText(/see me/i)).toBeTruthy()
+  })
+
+  it('should render the error message', () => {
+    const { queryByText, update } = render(
+      <ErrorBoundary>
+        <ChildWithError />
+      </ErrorBoundary>
+    )
+    expect(queryByText(/wrong/i)).toBeTruthy()
+
+    update(
+      <ErrorBoundary message='Custom error message'>
+        <ChildWithError />
+      </ErrorBoundary>
+    )
+    expect(queryByText(/error/i)).toBeTruthy()
+  })
+})
